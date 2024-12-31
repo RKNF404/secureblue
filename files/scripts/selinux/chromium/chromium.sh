e11718cb083c691c8160ec886f0822368b962157
Root <175176948+RKNF404@users.noreply.github.com>
2024-12-31 14:09:17 -0500

    chore: use new directory for selinux

--- files/scripts/selinux/chromium/chromium.sh
+++ files/scripts/selinux/chromium/chromium.sh
@@ -7,4 +7,4 @@ set -x
 make -f /usr/share/selinux/devel/Makefile chromium.pp || exit
 /usr/sbin/semodule -i chromium.pp
 
-/sbin/restorecon -F -R -v /usr/lib/chromium-browser/
+/sbin/restorecon -F -R -v /usr/lib/hardened-chromium/
